6d778d313d65b4e0ea0fd44c29d2a1079b9e7553
surekha25 <surekhasuri25@gmail.com>
2026-05-02 18:13:32 +0530

    python basics

---
+++ loops.py
@@ -0,0 +1,45 @@
+# value = 0
+
+# while value < 10:
+#     if value==5:
+         # Increment is very important
+#         value = value + 1
+#         continue
+#     print(value)
+#     value = value + 1
+
+sample= ["server1", "server2", "server3", "server4"]
+
+# idx = 0
+
+# while idx < len(sample):
+#     print(sample[idx])
+#     idx += 1 # idx = idx + 1
+
+# in -> membership operator (checks whether that element is present or not)
+# print(1 in sample)
+# print("server1" in sample)
+      
+# idx -> iterable
+# sample -> iterator
+# for idx in sample:
+#     print(idx)
+# print(idx)
+
+# access elements inside a list with index using for loop
+# range, enumerate
+# print(list(range(5)))
+
+for idx in range(len(sample)):
+    ele = sample[idx]
+    print(idx, ele)
+
+for val in enumerate(sample):
+    print(val)
+
+# tuple unpacking
+a, b = (1, 2)
+print(a, b)
+
+for idx,val in enumerate(sample):
+    print(idx, val)
